8183714c080d45d299f9359bda6fc63cee5678d2
QuentinGeerts <quentin.geerts@bstorm.be>
2022-10-06 16:39:59 +0200

    One-Way Binding

--- src/app/demos/components/demo1/demo1.component.ts
+++ src/app/demos/components/demo1/demo1.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Demo1Component implements OnInit {
 
-  constructor() { }
+  maVariable1: string | undefined;
+  maVariable2!: string; // definite assignment assertion
+  maVariable3: string = "Bonjour";
+  maVariable4: number = 0;
+  declare maVariable5: boolean; // ambient declaration
 
-  ngOnInit(): void {
+  maVariable6: null = null;
+  maVariable7: undefined;
+  maVariable8: any;
+  maVariable9: any[] = [12, "a", () => { }, true];
+
+  constructor () { }
+
+  ngOnInit (): void {
+
+    this.maVariable1 = "Coucou";
+    this.maVariable2 = "Hello";
+
+    this.maVariable5 = true;
+
+    this.maVariable8 = () => "Hello";
   }
-
 }
