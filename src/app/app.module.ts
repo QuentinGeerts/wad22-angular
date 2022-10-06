47409ea79210bd68fe621f671f4c81d8b0ace63a
QuentinGeerts <quentin.geerts@bstorm.be>
2022-10-06 13:37:32 +0200

    Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { FourofourComponent } from './shared/fourofour/fourofour.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    FourofourComponent,
   ],
   imports: [
     BrowserModule,
