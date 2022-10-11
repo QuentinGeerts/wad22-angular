452369ed8ebd7d629958d03ea4222307242aaedb
QuentinGeerts <quentin.geerts@bstorm.be>
2022-10-11 11:21:46 +0200

    Event binding

--- src/app/demos/components/demo3/demo3.component.ts
+++ src/app/demos/components/demo3/demo3.component.ts
@@ -8,17 +8,25 @@ import { Component, OnInit } from '@angular/core';
 export class Demo3Component implements OnInit {
 
   variable!: string;
+  texte!: string;
 
   constructor () { }
 
   ngOnInit (): void {
 
     this.variable = "Hello";
+    this.texte = "Hello";
 
   }
 
   maMethode (texte: string): void {
     this.variable = texte;
+  }
+
+  changeValue (event: Event): void {
+    let content = "Voici mon contenu"; // Variable locale
+    // console.log((event.target as HTMLInputElement).value);
+    this.texte = (event.target as HTMLInputElement).value;
   }
 
 }
